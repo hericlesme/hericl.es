58ab531d48c2e1c8c3648af7419d6aac866e314b
Héricles Emanuel <hericles.me@gmail.com>
2020-02-08 08:56:29 -0300

    desktop version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Cards from './components/Cards';
+import profile from './assets/profile.jpg';
+import {
+  Github,
+  Linkedin,
+  Twitter,
+  Behance,
+  Dribbble,
+  Instagram
+} from './components/svg';
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <img src={profile}/>
+        <div className="headline">
+          <p className="title">Hello! I'm Héricles.</p>
+          <p>
+          A curious software enginner trying to build the future I want to live.
+          </p>
+        </div>
+      </div>
+      <div className="links">
+          <a>
+            <Github/>
+            GitHub
+          </a>
+          <a>
+            <Linkedin/>
+            LinkedIn
+            </a>
+          <a>
+            <Twitter/>
+            Twitter
+            </a>
+          <a>
+            <Behance/>
+            Behance
+            </a>
+          <a>
+            <Dribbble/>
+            Dribbble
+            </a>
+          <a>
+            <Instagram/>
+            Instagram
+            </a>
+      </div>
+      <div className="about">
+
+      </div>
     </div>
   );
 }
