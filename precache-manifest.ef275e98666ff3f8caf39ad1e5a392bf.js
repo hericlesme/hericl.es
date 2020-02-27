5476eb32616a27e487aee01dc9b6fa3e9c297a5d
Héricles Emanuel <hericles.me@gmail.com>
2020-02-27 14:18:31 -0300

    Updates

---
+++ precache-manifest.ef275e98666ff3f8caf39ad1e5a392bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a76704494e199a92843c8606609080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66e5fde64d53d61ea12",
+    "url": "/static/css/main.5d576462.chunk.css"
+  },
+  {
+    "revision": "bc5445ce89ee17df4e48",
+    "url": "/static/js/2.dc9f7bc3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.dc9f7bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b66e5fde64d53d61ea12",
+    "url": "/static/js/main.27964c13.chunk.js"
+  },
+  {
+    "revision": "4e47a33556bd6e781f5c",
+    "url": "/static/js/runtime-main.bed57940.js"
+  },
+  {
+    "revision": "6108300826004b77046d1ffaf43dd98f",
+    "url": "/static/media/profile.61083008.jpg"
+  }
+]);
